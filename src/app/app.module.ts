ed71230bf96b0ed349a352d4e686eb054e39b152
Ivan Chaban <zanusilker@gmail.com>
2019-02-22 13:55:13 +0200

    Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './list/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
